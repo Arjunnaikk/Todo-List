63e244ed339897437b81d6ce783f071282bd37fe
Arjun Naik <143224276+Arjunnaikk@users.noreply.github.com>
2024-01-20 01:51:55 +0530

    Update script.js

--- todo/script.js
+++ todo/script.js
@@ -2,6 +2,7 @@ window.addEventListener('load',()=>{
     const form = document.querySelector("#new-task-form")
     const input = document.querySelector("#task-input")
     const list_el = document.querySelector(".list")
+    const lst_el = document.querySelector(".lst")
 
     const textToSave = document.querySelector(".text")
 
@@ -19,7 +20,7 @@ window.addEventListener('load',()=>{
             // Create a new task element
             let newTask = document.createElement("lst");
             newTask.innerHTML =  newTask.innerHTML + `<div class="box">
-            
+            <div class="done"><button><img src="done.svg" alt=""></button></div>
                 <input type="textarea" class="text" value="${task}" readonly>
                 <div class="but flex">
                 <div class="but-1"><button>Edit</button></div>
@@ -35,65 +36,67 @@ window.addEventListener('load',()=>{
             input.value = "";
         }
     })
+    
 
-    // Using event delegation to handle "Edit" button clicks
     list_el.addEventListener("click", (event) => {
-        const targetButton = event.target.closest(".but-1 button");
+        const targetButton = event.target.closest("button");
         const textToSave = targetButton.closest(".box").querySelector(".text");
-        if (targetButton && targetButton.innerText.toLowerCase() === "edit") {
-            console.log('Edit button clicked');
-            
-            targetButton.innerText = "Save"
-            // Assuming textToSave is an input element you want to make editable
 
-            // Remove the readonly attribute
-            if (textToSave) {
-                textToSave.removeAttribute("readonly");
+        if (targetButton) {
+            const action = targetButton.innerText.toLowerCase();
+
+            if (action === "edit") {
+                console.log('Edit button clicked');
+                targetButton.innerText = "Save";
+
+                // Remove the readonly attribute
+                if (textToSave) {
+                    textToSave.removeAttribute("readonly");
+                }
             }
-        }
-        else{
-            targetButton.innerText = "Edit"
-            textToSave.setAttribute("readonly","readonly")
-        }
-    });
-
-    //Delete the content
-    // list_el.addEventListener("click", (event) => {
-    //     const deleteButton = event.target.closest(".but-2");
-    //     if(deleteButton){
-    //         console.log(deleteButton);
-    //         const deletecontent = deleteButton.closest(".box")
-    //         if(deletecontent){
-    //             list_el.removeChild(deletecontent)
-    //         }
-    //     }
-        
-    // });
-
-    list_el.addEventListener("click", (event) => {
-        const deleteButton = event.target.closest(".but-2");
+                else if(action === "save"){
+                    targetButton.innerText = "Edit"
+                    textToSave.setAttribute("readonly","readonly")
+                }
+            else if (action === "delete") {
+                const deleteContent = targetButton.closest(".box");
     
-        if (deleteButton) {
-            console.log(deleteButton);
+                if (deleteContent) {
+                    // Check if deleteContent is a child of lst
+                    const lstElement = deleteContent.closest("lst");
     
-            const deleteContent = deleteButton.closest(".box");
-    
-            if (deleteContent) {
-                list_el.removeChild(deleteContent);
+                    if (lstElement) {
+                        lstElement.removeChild(deleteContent);
+                    } else {
+                        console.error("lst not found for deleteContent");
+                    }
+                } else {
+                    console.error("Element not found");
+                }
             }
         }
     });
-    
 
+
+    list_el.addEventListener("click",(event)=>{
+        const circle = event.target.closest("button");
+        const textToChange = circle.closest(".box").querySelector(".text")
+
+        if (circle) {
+            const circleImg = circle.querySelector("img");
+            if (circleImg) {
+                if (circleImg.src.endsWith("done.svg")) {
+                    textToChange.style.color = "#0dff00";
+                    circleImg.src = "complete.svg";
+                } else {
+                    textToChange.style.color = "red";
+                    circleImg.src = "done.svg";
+                }
+            }
+        }
+
+    })
 
 });
 
-    // const edit = document.querySelector("lst");
-    // console.log(edit);
-    // let edit = document.querySelector(".lst").getElementsByTagName(".but-1")
-    // edit.addEventListener("click",()=>{
-    //     console.log('Clickkk');
-    // })
 
-    
-// })
